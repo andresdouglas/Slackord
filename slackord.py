b328350c18a14c2f665545bb78e9e41c2724de41
mfc <mfcirit@gmail.com>
2020-01-24 00:40:08 +0300

    works really well :)

--- slackord.py
+++ slackord.py
@@ -1,94 +1,79 @@
-# Slackord 1.0 by Thomas Loupe
-
 import asyncio
 import datetime
 import discord
 from discord.ext import commands
 import json
-import time
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
-from tkinter import *
-from tkinter import simpledialog
+import os
+import urllib.request
+from discord import File
+import glob
 
 bot = commands.Bot(command_prefix='!')
 
+id_to_username = {}
 
-# Create the popup window to enter the bot's token.
-def SpawnTokenWindow():
-    botToken = simpledialog.askstring(
-        "Enter Token", "Please enter your Discord bot token here")
-    if not botToken:
-        frameBox.insert(tk.END, 'User cancelled or no token entered.')
-        frameBox.yview(tk.END)
-    else:
-        # TODO: Thread this process with bot.start().
-        bot.run(botToken)
+filename = "C:/projects/Slackord/export/users.json"
+with open(filename, encoding='utf-8') as f:
+    for user in json.load(f):
+        required_fields_image = ["id", "name"]
+        if all(x in user for x in required_fields_image):
+            id_to_username[user["id"]] = user["name"]
 
 
-def Output():
-    filename = tk.filedialog.askopenfilename()
-    with open(filename) as f:
-        frameBox.insert(
-            tk.END, 'Slackord will post the following messages to your desired Discord channel:')
-        frameBox.yview(tk.END)
-        for message in json.load(f):
-            # Print the messages we'll output from Slack JSON file to Discord to GUI window.
-            if "user_profile" in message and 'ts' in message and 'text' in message:
-                time = datetime.datetime.fromtimestamp(
-                    float(message['ts'])).strftime('%Y-%m-%d %H:%M:%S')
-                rn = (message['user_profile']['real_name'])
-                mess = (message['text'])
-                guiMessageToSend = (time) + (": ") + (rn) + (" - ") + (mess)
-                frameBox.insert(tk.END, (guiMessageToSend))
-                frameBox.yview(tk.END)
-                frameBox.insert(tk.END, ' ')
-                frameBox.yview(tk.END)
+# When !bort is typed in a channel, iterate through the JSON file and post each message.
+@bot.command(pass_context=True)
+async def bort(ctx):
+    folder_name = "C:/projects/Slackord/export/av-workshop/"
+    os.chdir(folder_name)
+    # filename = "C:/projects/Slackord/export/adas-project/2019-09-04.json"
+    for file in glob.glob("*.json"):
+        filename = folder_name + file
+        print("Processing " + filename)
+        with open(filename, encoding='utf-8') as f:
+            for message in json.load(f):
+                # Images
+                required_fields_image = ["files", "user", "ts"]
+                if all(x in message for x in required_fields_image):
+                    time = datetime.datetime.fromtimestamp(
+                        float(message['ts'])).strftime('%Y-%m-%d %H:%M:%S')
+                    rn = id_to_username[message['user']]
+                    for uploaded_file in message["files"]:
+                        pretty_type = str(uploaded_file["pretty_type"]).lower()
 
-        # When !slackord is typed in a channel, iterate through the JSON file and post each message.
-        @bot.command(pass_context=True)
-        async def slackord(ctx):
-            frameBox.insert(tk.END, 'Posting messages into Discord!')
-            frameBox.yview(tk.END)
-            with open(filename) as f:
-                for message in json.load(f):
-                    if "user_profile" in message and 'ts' in message and 'text' in message:
-                        time = datetime.datetime.fromtimestamp(
-                            float(message['ts'])).strftime('%Y-%m-%d %H:%M:%S')
-                        rn = (message['user_profile']['real_name'])
-                        mess = (message['text'])
-                        messageToSend = (time) + (": ") + \
-                            (rn) + (" - ") + (mess)
-                        await ctx.send(messageToSend)
-                        # Output to the GUI that the message was posted.
-                        frameBox.insert(tk.END, 'Message posted!')
-                        frameBox.yview(tk.END)
+                        image_types = ["jpeg", "jpg", "png", "gif", "bmp", "pdf", "docx", "xlsx", "pptx", "txt", "md",
+                                       "mp3"]
+                        if not any(x in pretty_type for x in image_types):
+                            continue
+                        uploaded_filename = uploaded_file["name"]
+                        url = uploaded_file["url_private"]
+                        local_filename, headers = urllib.request.urlretrieve(url)
+
+                        better_name = local_filename + "." + pretty_type
+                        os.rename(local_filename, better_name)
+                        local_filename = better_name
+
+                        print("local_filename = " + local_filename)
+                        messageToSend = time + " " + rn + " " + uploaded_filename
+                        await ctx.send(content=messageToSend, file=File(local_filename))
+                        print(messageToSend + " " + url)
+
+                # Normal Messages
+                required_fields_normal_message = ["user_profile", "ts", "text"]
+                if all(x in message for x in required_fields_normal_message):
+                    time = datetime.datetime.fromtimestamp(
+                        float(message['ts'])).strftime('%Y-%m-%d %H:%M:%S')
+
+                    rn = (message['user_profile']['real_name'])
+                    mess = (message['text'])
+
+                    if "attachments" in message:
+                        for attachment in message["attachments"]:
+                            mess += " " + attachment["fallback"]
+
+                    messageToSend = (time) + (": ") + \
+                                    (rn) + (" - ") + (mess)
+                    await ctx.send(content=messageToSend)
+                    print(messageToSend)
 
 
-# Begin tkinter setup.
-# Create tkinter instance.
-window = tk.Tk()
-# Set window title.
-window.title("Slackord 1.0a by Thomas Loupe")
-# Set window size.
-window.geometry("500x300")
-# Disallow window resizing.
-window.resizable(False, False)
-
-# Set up the file browse button.
-tk.Button(window, text="Step 1: Select JSON File", command=Output).pack()
-
-# Set up the token button
-tk.Button(window, text="Step 2: Enter Bot Token",
-          command=SpawnTokenWindow).pack()
-
-# Create an empty text field and scrollbar to print updates and messages to.
-frame = Frame(window)
-scrollbar = Scrollbar(window)
-scrollbar.pack(side=RIGHT, fill=Y)
-frameBox = Listbox(window, yscrollcommand=scrollbar.set)
-frameBox.pack(fill='both', expand=True)
-scrollbar.config(command=frameBox.yview)
-
-# Keep the application running unless closed.
-window.mainloop()
+bot.run("NjY5NDg3ODA2NDE4MjU1ODk2.XinqGg.Ow6y7s7YmV5SCtmx1SSV83dwpYY")
